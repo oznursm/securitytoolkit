5a7befe3473b711103330340d1f4bbc87b50449f
oznursm <ooznrusiram@gmai.com>
2024-07-15 19:11:54 +0300

    second

---
+++ functions.py
@@ -0,0 +1,200 @@
+import os
+import pyfiglet
+import time
+import requests
+
+
+def secim():
+    os.system("clear")
+    figlet_oznur = pyfiglet.figlet_format("AĞ GÜVENLİĞİ ARAÇ KİTİ", font="larry3d")
+    print(figlet_oznur + "version 1.0\n\n")
+
+    print("1 - Mac Adresi Değiştirmek")
+    print("2 - Bilgi Toplama")
+    print("3 - Ağ Taramaları")
+    print("4 - Hedefte Çıkmış Zafiyet Taraması")
+    print("5 - Hedefte Web Zafiyeti Bulma (XSS, SQL)")
+    print("6 - Görsel Analiz")
+    print("7 - Wordpress Siteleri Tarama")
+    print("8 - Kişiye Özel Wordlist Oluşturma ")
+    print("9 - Rustscan")
+    print("10 - TCPDump")
+    print("11 - Docker ile OpenVAS")
+    print("12 - Docker Ollama ile Zafiyet Analizi")
+
+def getMac():
+    kelime = input("\nMac adresini değiştirmek istediğin interface giriniz(eth0,wlan0): ")
+    os.system("macchanger -r " + kelime)
+
+def infoinfo():
+    os.system("clear")
+    print("1 - Dmitry Aracı İle Bilgi Toplama")
+    print("2 - TheHarvester Aracı İle Bilgi Toplama")
+    print("3 - Local Ağdaki Cihazları Göster")
+    print("4 - Hedef Adreste Güvenlik Duvarı Kontrol")
+    print("5 - Dizin Taraması")
+    print("6 - DNS Taraması")
+
+    bilgisecim = input("\nBilgi Toplanmak İstenen Aracı Seçiniz: ")
+
+    if bilgisecim == "1":
+        dmtirysorgu = input("Sorgulancak Alan Adını girin(www.site.com): ")
+        os.system(f"dmitry -winsep {dmtirysorgu}")
+
+    elif bilgisecim == "2":
+        harvester = input("Sorgulancak Alan Adını girin: ")
+        os.system(f"theHarvester -d {harvester} -l 500 -b all")
+
+    elif bilgisecim == "3":
+        os.system("netdiscover")
+
+    elif bilgisecim == "4":
+        wafwoof = input("Alan Adını Giriniz: ")
+        os.system(f"wafw00f -a {wafwoof}")
+
+    elif bilgisecim == "5":
+        dirb = input("Dizin Sorgusu Yapılacak Sitesi Linki: ")
+        os.system(f"dirb {dirb}")
+
+    elif bilgisecim == "6":
+        dnsenum = input("DNS Sorgusu Yapılacak Adres: ")
+        os.system(f"dnsenum {dnsenum}")
+
+    else:
+        print("Hatalı Seçim Yaptınız! Tekrar Deneyiniz")
+
+def nmapscan():
+    os.system("clear")
+    print("1 - Nmap Servis ve Versiyon Taraması")
+    print("2 - Nmap İle Script Taraması")
+    print("3 - Nmap ile Ayrıntılı Tarama")
+    print("4 - Nmap ile Tüm TCP Portları Tara")
+    print("5 - Nmap ile Tüm UDP Portları Tara")
+    print("6 - Nmap ile Hedef İşletim Sistemi Tespiti")
+
+    nmapsorgu = input("\nYapmak İstediğiniz İşlemi Seçiniz: ")
+
+    if nmapsorgu == "1":
+        nmap = input("Tarama Yapılacak IP adresi: ")
+        print("Lütfen Bekleyiniz...")
+        os.system(f"nmap -sS -sV {nmap}")
+        time.sleep(1000)  # ------------------- geçici çözüm olarak sleep iş görür fakat yeni çözüm şart
+
+    elif nmapsorgu == "2":
+        nmap = input("Tarama Yapılacak IP adresi: ")
+        print("Lütfen Bekleyiniz...")
+        os.system(f"nmap -sC {nmap}")
+
+    elif nmapsorgu == "3":
+        nmap = input("Tarama Yapılacak IP adresi: ")
+        print("Lütfen Bekleyiniz...")
+        os.system(f"nmap -A {nmap}")
+
+    elif nmapsorgu == "4":
+        nmap = input("Tarama Yapılacak IP adresi: ")
+        print("Lütfen Bekleyiniz...")
+        os.system(f"nmap -sT {nmap}")
+
+    elif nmapsorgu == "5":
+        nmap = input("Tarama Yapılacak IP adresi: ")
+        print("Lütfen Bekleyiniz...")
+        os.system(f"nmap -sU {nmap}")
+
+    elif nmapsorgu == "6":
+        nmap = input("Tarama Yapılacak IP adresi: ")
+        print("Lütfen Bekleyiniz...")
+        os.system(f"nmap -O {nmap}")
+
+def searchsploit():
+    os.system("clear")
+    searchplt = input("Zafiyet Aranacak Anahtar Kelime(Ör: Vsftpd): ")
+    os.system(f"searchsploit {searchplt}")
+
+def niktoscan():
+    os.system("clear")
+    print("1 - Web Sitesine Yönelik Standart Tarama")
+    print("2 - SQL Injection Taraması")
+    print("3 - XSS Taraması")
+
+    niktosearch = input("\nYapmak İstediğiniz İşlemi Seçiniz: ")
+
+    if niktosearch == "1":
+        searchnikto = input("Tarama Yapılacak URL Adresini Giriniz: ")
+        os.system(f"nikto -h {searchnikto}")
+
+    elif niktosearch == "2":
+        searchnikto = input("Tarama Yapılacak URL Adresini Giriniz: ")
+        os.system(f"nikto -h {searchnikto} -Cgidirs none -Tuning 9")
+
+    elif niktosearch == "3":
+        searchnikto = input("Arama Yapılacak URL Adresini Giriniz: ")
+        os.system(f"nikto -h {searchnikto} -Cgidirs none -Tuning 4")
+
+def exiftool():
+    os.system("clear")
+    exiftool = input("Analiz Yapılacak Resim Yolu: ")
+    os.system(f"exiftool {exiftool}")
+
+def wordpress():
+    os.system("clear")
+    print("1 - Hedef Siteye Yönelik Genel Tarama")
+    print("2 - Hedef Sitenin Eklentilerini Tespit Etmek")
+    print("3 - Kullanılan Tema Üzerindeki Açıklar")
+    print("4 - Sitede Kullanılan Tema Bilgisi")
+
+    wordinput = input("\nYapmak İstediğiniz İşlemi Seçiniz: ")
+
+    if wordinput == "1":
+        scanpress = input("Taranacak Site: ")
+        os.system(f"wpscan --url {scanpress}")
+
+    elif wordinput == "2":
+        scanpress = input("Taranacak Site: ")
+        os.system(f"wpscan --url {scanpress} --enumerate p")
+
+        eklentitarama = input("Bulunan Eklentilerin Zafiyetleri Taransın mı? (e veya h): ")
+        if eklentitarama == "e":
+            os.system(f"wpscan --url {scanpress} --enumerate ap")
+
+        elif eklentitarama == "h":
+            print("Çıkış Yapıldı")
+        else:
+            print("Hatalı İşlem Yaptınız")
+
+    elif wordinput == "3":
+        scanpress = input("Taranacak Site: ")
+        os.system(f"wpscan --url {scanpress} --enumerate at")
+
+    elif wordinput == "4":
+        scanpress = input("Taranacak Site: ")
+        os.system(f"wpscan --url {scanpress} --enumerate t")
+
+def wordlist():
+    os.system("clear")
+    print("Oluşturulacak Wordlist için Parametleri Giriniz\n")
+    min = input("Wordlistiniz Minimum Kaç Karakter İçersin: ")
+    max = input("Wordlistiniz Maximum Kaç Karakter İçersin: ")
+    icerik = input("Wordlistiniz Hangi Karakterleri İçersin: ")
+    dosyaismi = input("Wordlist Dosyanızın İsmi: ")
+    os.system(f"crunch {min} {max} {icerik} -o {dosyaismi}")
+
+def rustscan():
+    os.system("clear")
+    ip_address = input("Tarama Yapılacak IP Adresini Giriniz: ")
+    os.system(f"rustscan -a {ip_address}")
+
+def tcpdump():
+    os.system("clear")
+    interface = input("Hangi Arayüzden Dinleme Yapılsın (eth0, wlan0, vb.): ")
+    os.system(f"tcpdump -i {interface}")
+
+def docker_openvas():
+    os.system("clear")
+    os.system("sudo docker run -d -p 443:443 --name openvas mikesplain/openvas")
+    print("OpenVAS Docker container started. Access it at https://localhost:443")
+
+def docker_ollama():
+    os.system("clear")
+    os.system("docker pull ollama/ollama")
+    os.system("sudo docker run -d -v ollama:/root/.ollama -p 11437:11434 --name ollama5 ollama/ollama")
+    os.system("docker exec -it ollama5 ollama run llama3")
---
+++ start.py
@@ -0,0 +1,102 @@
+import gi
+import functions as func
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+class MainWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="AĞ GÜVENLİĞİ ARAÇ KİTİ")
+        self.set_border_width(10)
+
+        grid = Gtk.Grid()
+        self.add(grid)
+
+        button1 = Gtk.Button(label="Mac Adresi Değiştir")
+        button1.connect("clicked", self.on_mac_change_clicked)
+        grid.attach(button1, 0, 0, 1, 1)
+
+        button2 = Gtk.Button(label="Bilgi Toplama")
+        button2.connect("clicked", self.on_info_gathering_clicked)
+        grid.attach(button2, 1, 0, 1, 1)
+
+        button3 = Gtk.Button(label="Ağ Taramaları")
+        button3.connect("clicked", self.on_nmap_scan_clicked)
+        grid.attach(button3, 2, 0, 1, 1)
+
+        button4 = Gtk.Button(label="SearchSploit")
+        button4.connect("clicked", self.on_searchsploit_clicked)
+        grid.attach(button4, 0, 1, 1, 1)
+
+        button5 = Gtk.Button(label="Nikto Tarama")
+        button5.connect("clicked", self.on_niktoscan_clicked)
+        grid.attach(button5, 1, 1, 1, 1)
+
+        button6 = Gtk.Button(label="Görsel Analiz(Exiftool)")
+        button6.connect("clicked", self.on_exiftool_clicked)
+        grid.attach(button6, 2, 1, 1, 1)
+
+        button7 = Gtk.Button(label="Wordpress Tarama")
+        button7.connect("clicked", self.on_wordpress_clicked)
+        grid.attach(button7, 0, 2, 1, 1)
+
+        button8 = Gtk.Button(label="Wordlist Oluşturma")
+        button8.connect("clicked", self.on_wordlist_clicked)
+        grid.attach(button8, 1, 2, 1, 1)
+
+        button9 = Gtk.Button(label="Rustscan")
+        button9.connect("clicked", self.on_rustscan_clicked)
+        grid.attach(button9, 2, 2, 1, 1)
+
+        button10 = Gtk.Button(label="TCPDump")
+        button10.connect("clicked", self.on_tcpdump_clicked)
+        grid.attach(button10, 0, 3, 1, 1)
+
+        button11 = Gtk.Button(label="Docker ile OpenVAS")
+        button11.connect("clicked", self.on_docker_openvas_clicked)
+        grid.attach(button11, 1, 3, 1, 1)
+
+        button12 = Gtk.Button(label="Docker ile Ollama")
+        button12.connect("clicked", self.on_docker_ollama_clicked)
+        grid.attach(button12, 2, 3, 1, 1)
+
+    def on_mac_change_clicked(self, widget):
+        func.getMac()
+
+    def on_info_gathering_clicked(self, widget):
+        func.infoinfo()
+
+    def on_nmap_scan_clicked(self, widget):
+        func.nmapscan()
+
+    def on_searchsploit_clicked(self, widget):
+        func.searchsploit()
+
+    def on_niktoscan_clicked(self, widget):
+        func.niktoscan()
+
+    def on_exiftool_clicked(self, widget):
+        func.exiftool()
+
+    def on_wordpress_clicked(self, widget):
+        func.wordpress()
+
+    def on_wordlist_clicked(self, widget):
+        func.wordlist()
+
+    def on_rustscan_clicked(self, widget):
+        func.rustscan()
+
+    def on_tcpdump_clicked(self, widget):
+        func.tcpdump()
+
+    def on_docker_openvas_clicked(self, widget):
+        func.docker_openvas()
+
+    def on_docker_ollama_clicked(self, widget):
+        func.docker_ollama()
+
+win = MainWindow()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
